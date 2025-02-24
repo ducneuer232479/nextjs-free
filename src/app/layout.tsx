68d5ef153ef8290a60ba99c5f95ab7e1551a01c2
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-02-24 19:18:01 +0700

    18. Quản lý Authentication trong Next.js. Điều mà ít người nói với bạn!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import Header from '@/components/header'
 import { Toaster } from '@/components/ui/toaster'
+import AppProvider from '@/app/AppProvider'
+import { cookies } from 'next/headers'
 
 const inter = Inter({ subsets: ['vietnamese'] })
 
@@ -32,6 +34,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const cookieStore = cookies()
+  const sessionToken = cookieStore.get('sessionToken')
+
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={`${inter.className}`}>
@@ -43,7 +48,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          {children}
+          <AppProvider initialSessionToken={sessionToken?.value}>
+            {children}
+          </AppProvider>
         </ThemeProvider>
       </body>
     </html>
---
+++ src/app/me/profile.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useAppContext } from '@/app/AppProvider'
+import envConfig from '@/config'
+import React, { useEffect } from 'react'
+
+const Profile = () => {
+  const { sessionToken } = useAppContext()
+
+  useEffect(() => {
+    const fetchRequest = async () => {
+      const result = await fetch(
+        `${envConfig.NEXT_PUBLIC_API_ENDPOINT}/account/me`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${sessionToken}`
+          }
+        }
+      ).then(async (res) => {
+        const payload = await res.json()
+
+        const data = {
+          status: res.status,
+          payload
+        }
+
+        if (!res.ok) {
+          throw data
+        }
+
+        return data
+      })
+      console.log('profile', result)
+    }
+
+    fetchRequest()
+  }, [sessionToken])
+
+  return <div>Profile</div>
+}
+
+export default Profile
