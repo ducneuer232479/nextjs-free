25d267612788e8d4130281d6de2e1952f213c1c7
ducneuer232479 <tuanduc.vu249@gmail.com>
2025-03-07 21:00:47 +0700

    35. SEO title và description

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Metadata } from 'next'
 import { cookies } from 'next/headers'
 import Profile from '@/app/me/profile'
 import accountApiRequest from '@/apiRequests/account'
 import ProfileForm from '@/app/me/profile-form'
+
+export const metadata: Metadata = {
+  title: 'Hồ sơ người dùng',
+  description: 'Được tạo bởi Đức dev'
+}
 
 const MeProfile = async () => {
   const cookieStore = cookies()
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,11 +1,32 @@
+import React, { cache } from 'react'
 import productApiRequest from '@/apiRequests/product'
 import Image from 'next/image'
-import React from 'react'
+import { Metadata, ResolvingMetadata } from 'next'
 
-const ProductDetail = async ({ params }: { params: { id: string } }) => {
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+const getDetail = cache(productApiRequest.getDetail)
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { payload } = await getDetail(Number(params.id))
+  const product = payload.data
+
+  return {
+    title: product.name,
+    description: product.description
+  }
+}
+
+const ProductDetail = async ({ params, searchParams }: Props) => {
   let product = null
   try {
-    const { payload } = await productApiRequest.getDetail(Number(params.id))
+    const { payload } = await getDetail(Number(params.id))
     product = payload.data
   } catch (error) {}
 
